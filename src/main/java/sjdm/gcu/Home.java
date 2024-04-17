23b5ac95c0195e0c82fc2aaf01c553f22c2fedf2
Joshua Jeffrey Alvarez <alvarezjoja77@gmail.com>
2024-04-16 21:03:13 -0700

    test

--- src/main/java/sjdm/gcu/Home.java
+++ src/main/java/sjdm/gcu/Home.java
@@ -10,7 +10,7 @@ public class Home {
 
 	@GetMapping("/")
 	public String home() {
-		// view resolver takes the view name returned by the controller ("home") and maps it to a specific view file (home.html)
+		// view resolver takes the view name returned by the controller ("home") and maps it to a specific view file (home.html) ()
 		// returning "home" from the controller method would result in the view resolver looking for a file named home.html in the templates directory.
 		return "home"; // View name
 	}
